37771a3093e157b9b7899ee952191316bf0f0afc
Rob-2021 <dragrober.rar@gmail.com>
2023-08-14 11:28:12 -0400

    dia 1

---
+++ pythonPrincipiantes/00_holamundo.py
@@ -0,0 +1,9 @@
+#Hola mundo
+print("Hola Mundo")
+
+#Consultar el tipo de dato
+print(type('Hola mundo')) #tipo 'string'
+print(type(5)) #tipo 'int'
+print(type(1.5)) #tipo 'float'
+print(type(1+ 1j)) #tipo 'complex'
+print(type(True)) #tipo 'bool'
---
+++ pythonPrincipiantes/01_variables.py
@@ -0,0 +1,3 @@
+esto_es_un_int = 34
+
+print(esto_es_un_int)
