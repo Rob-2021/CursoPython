1e79d17cf8f0bf816d268b721d3b706efc5f9091
Rob-2021 <dragrober.rar@gmail.com>
2023-09-07 10:52:22 -0400

    oeradores

--- pythonPrincipiantes/02_operadores.py
+++ pythonPrincipiantes/02_operadores.py
@@ -1,4 +1,4 @@
-
+# Operadores aritmeticos
 print(3 + 4)
 print(3 - 4)
 print(3 * 4)
@@ -11,3 +11,25 @@ print(2 ** 3 + 3 - 7 / 1 // 4)
 print('Hola' + 'Python')
 print('Hola' + str(5))
 print('Hola ' * 5)
+
+my_float = 2.5 * 2
+print('hola ' * int(my_float))
+
+
+# operadores comparativos
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print( 3 == 4)
+print(3 != 4)
+print(3 > 4 > 2)
+
+print('hola' > 'python') # ordenacion alfabetica por ASCII
+
+# Operadores Logicos
+
+print(3 > 4 and 'hola' > 'python')
+print(3 > 4 or 'hola' > 'python')
+print(not(3 > 4))
