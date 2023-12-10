c6eedcce30e246a239315c12a0fdad2f98693513
Rob-2021 <dragrober.rar@gmail.com>
2023-12-09 21:11:23 -0400

    sets

---
+++ pythonPrincipiantes/06_sets.py
@@ -0,0 +1,44 @@
+# Sets
+
+my_set = set()
+my_other_set = {}
+
+print(type(my_set))
+print(type(my_other_set)) # inicialmente es un diccionario
+
+my_other_set = {'Brais', 'Moure', 35}
+print(type(my_other_set))
+
+print(len(my_other_set))
+
+my_other_set.add('MoureDev')
+
+print(my_other_set) # un set no es una estructura ordenada
+
+my_other_set.add('MoureDev') # un set no admite repetidos
+
+print(my_other_set)
+
+print('Moure' in my_other_set)
+print('Mouri' in my_other_set)
+
+my_other_set.remove('Moure')
+print(my_other_set)
+
+my_other_set.clear()
+print(len(my_other_set))
+
+del my_other_set
+#print(my_other_set) NameError: name 'my_other_set' is not defined
+
+my_set = {'Brais', 'Moure', 35}
+my_list = list(my_set)
+print(my_list)
+print(my_list[0])
+
+my_other_set = {'kotlin', 'swift', 'python'}
+
+my_new_set = my_set.union(my_other_set)
+print(my_new_set.union(my_new_set).union({'java', 'c#'}))
+
+print(my_new_set.difference(my_set))
