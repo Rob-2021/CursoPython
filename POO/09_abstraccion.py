e2389dd8b7ab5f820a27f4c2b12e7f0741aa6022
Rob-2021 <dragrober.rar@gmail.com>
2024-02-05 08:36:07 -0400

    abstraccion

---
+++ POO/09_abstraccion.py
@@ -0,0 +1,18 @@
+# Abstraccion, es el proceso de ocultar los detalles de implementacion 
+#y mostrar solo la funcionalidad al usuario.
+
+class Auto:
+    def __init__(self):
+        self._estado = 'apagado'
+
+    def encender(self):
+        self._estado = 'encendido'
+        print('El auto esta encendido')
+
+    def conducir(self):
+        if self._estado == 'apagado':
+            self.encender()
+        print('Conduciendo el auto')
+
+mi_auto = Auto()
+mi_auto.conducir()
